c62b171adc24e84f72feb467941034ef6a1fd612
Justin M Wozniak <wozniak@mcs.anl.gov>
2026-04-29 11:56:57 -0500

    Better logging, error handling

--- agent-sh/run-single.zsh
+++ agent-sh/run-single.zsh
@@ -20,19 +20,20 @@ source $THIS/affinity.sh
 # Exit code:
 CODE=0
 
-msgv "START: RANK=%02i ZE_AFFINITY_MASK=$ZE_AFFINITY_MASK $CFG" $RANK
+msgv "START: RANK=%02i $AFFINITY_LABEL $CFG" $RANK
 
 # Use floating-point for time
 typeset -F SECONDS
 START=$SECONDS
 
 OUTFILE=${CFG%.cfg}.out
-if =agent $CFG >& $OUTFILE
+printf "run: RANK=%02i $AFFINITY_LABEL CFG=$CFG\n\n" $RANK > $OUTFILE
+if =agent $CFG >>& $OUTFILE
 then
   : OK
 else
-  msg "ERROR: RANK=$RANK"
-  CODE=1
+  CODE=$?
+  msgv "ERROR: RANK=%02i CODE=$CODE from agent" $RANK
 fi
 
 STOP=$SECONDS
