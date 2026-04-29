8da71a0afa1d435f8220cdfa0573c99689ca5b32
Justin M Wozniak <wozniak@mcs.anl.gov>
2026-04-29 11:57:19 -0500

    Another approach to affinity switching

--- agent-sh/affinity.sh
+++ agent-sh/affinity.sh
@@ -1,14 +1,34 @@
 
-# Source this to get ZE settings for given GPUs and TILES
+# Source this to get CUDA or ZE settings for given GPUs and TILES
 # Assumes RANK is set
+# Sets human-readable AFFINITY_LABEL for logging or whatever
 
-num_gpu=6 # 6
-num_tile=2 # 1
+num_gpu=4 # 6
+num_tile=1 # 1
 
-gpu_id=$((  ( RANK / num_tile ) % num_gpu ))
-tile_id=$((   RANK % num_tile   ))
+HOSTDOMAIN=$( hostname -d )
 
-unset EnableWalkerPartition
-export ZE_ENABLE_PCI_ID_DEVICE_ORDER=1
-export ZE_AFFINITY_MASK=$gpu_id.$tile_id
-export ZE_ENABLE_API_TRACING=0
+if [[ $HOSTDOMAIN == *aurora* ]]
+then
+
+  gpu_id=$((  ( RANK / num_tile ) % num_gpu ))
+  tile_id=$((   RANK % num_tile   ))
+
+  unset EnableWalkerPartition
+  export ZE_ENABLE_PCI_ID_DEVICE_ORDER=1
+  export ZE_AFFINITY_MASK=$gpu_id.$tile_id
+  export ZE_ENABLE_API_TRACING=0
+
+  AFFINITY_LABEL="ZE_AFFINITY_MASK=$ZE_AFFINITY_MASK"
+
+elif [[ $HOSTDOMAIN == *perlm* ]]
+then
+
+  export CUDA_VISIBLE_DEVICES=$(( RANK % num_gpu ))
+  AFFINITY_LABEL="CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+else
+  echo "affinity.sh: unknown host: $HOSTDOMAIN"
+  AFFINITY_LABEL="UNKNOWN"
+  return 1
+fi
