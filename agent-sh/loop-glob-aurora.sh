a7dabe1975ddef32d0190ee43d70e39c9dd0bb66
Justin Wozniak <woz@anl.gov>
2026-05-11 13:24:04 -0500

    Stop copying inputs

--- agent-sh/loop-glob-aurora.sh
+++ agent-sh/loop-glob-aurora.sh
@@ -4,7 +4,6 @@ set -eu
 # LOOP GLOB AURORA SH
 # All arguments passed to workflow
 # See README
-# DOES NOT WORK YET!
 
 THIS=${0:h:A}
 
@@ -20,8 +19,11 @@ fi
 # This is used by copy-inputs.sh
 export DIR_INPUTS=${1:A}
 
-export TURBINE_LOG=0
+export TURBINE_LOG=1
+
+export TURBINE_OUTPUT=${DIR_INPUTS:a}
 
 set -x
 which mpiexec agent swift-t
-swift-t -m pbs -n $PROCS -t i:copy-inputs.sh loop-glob.swift ${*}
+swift-t -m pbs -n $PROCS loop-glob.swift ${*}
+# -t i:copy-inputs.sh 
