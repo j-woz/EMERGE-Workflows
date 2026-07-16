73946a2ebc40e0ffc19723807c6486a26aff2ba9
Justin Wozniak <woz@anl.gov>
2026-07-16 11:37:53 -0500

    Add header

--- agent-py/insert-params-id.awk
+++ agent-py/insert-params-id.awk
@@ -1,6 +1,6 @@
 
 # INSERT PARAMS ID AWK
-# Inserts a params_id column into the CSV
+# See insert-params-id.sh
 
 NR == 1 { print "params_id," $0 }
 NR != 1 { print NR - 1   "," $0 }
--- agent-py/insert-params-id.sh
+++ agent-py/insert-params-id.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 set -eu
 
+# INSERT PARAMS ID
+# Copies the original CSV, inserting a leading column "params_id
+# that is simply a record number.
+
 THIS=${0:h:A}
 
 FILE_IN=$1
