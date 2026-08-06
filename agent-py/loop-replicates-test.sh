6764a2b89e491e77081a724b749159f923d12ba1
Justin Wozniak <woz@anl.gov>
2026-08-06 14:20:29 -0500

    More local testing features

--- agent-py/loop-replicates-test.sh
+++ agent-py/loop-replicates-test.sh
@@ -1,10 +1,37 @@
+#!/bin/zsh
+set -eu
 
 # LOOP REPLICATES TEST
 # Local test use case
 
+THIS=${0:A:h}
+source $THIS/utils.zsh
+
+export PYTHONPATH=$PWD
+export ADLB_DEBUG=0
+
+if (( ${#*} != 1 )) error "Provide PARAMS!"
+
+PARAMS=$1
+
 mkdir -pv /tmp/woz/exaepi
 
-cp -uv template.cfg urbanpop_nm.bin NM_Mar16.cases /tmp/woz/exaepi
+if ! which agent > /dev/null
+then
+  error "Add ExaEpi agent to PATH!"
+fi
 
-swift-t -p -n 10 loop-local-replicates.swift template.cfg test-3.csv 2 \
+WORK_DIR=/tmp/woz/exaepi
+cp -uv template.cfg urbanpop_nm.bin NM_Mar16.cases =agent $WORK_DIR
+NOOP=affinity-noop.sh
+AFFINITY=$WORK_DIR/affinity.sh
+if [[ ! -f        $AFFINITY ]] ||
+   [[   $NOOP -nt $AFFINITY ]] {
+  cp -v $NOOP     $AFFINITY
+  chmod u+x       $AFFINITY
+}
+
+PATH=$WORK_DIR:$PATH
+
+swift-t -p -n 10 loop-local-replicates.swift template.cfg $PARAMS 2 \
         urbanpop_nm.bin NM_Mar16.cases results.log
